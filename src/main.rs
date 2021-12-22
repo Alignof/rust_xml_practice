47eed452bf546b97831cd958df3ff7d6fd875860
Alingof <Alignof@outlook.com>
2021-12-23 00:11:49 +0900

    [add] add DOM parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use quick_xml::events::Event;
-use quick_xml::Reader;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let protain_id = "P0DTC2";
@@ -8,43 +5,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let xml = reqwest::get(url).await?.text().await?;
 
-    println!("{:#?}", xml);
+    let root: minidom::Element = xml.parse().unwrap();
 
-    let mut reader = Reader::from_str(&xml);
-    reader.trim_text(true);
-
-    let mut txt = Vec::new();
-    let mut buf = Vec::new();
-
-    loop {
-        match reader.read_event(&mut buf) {
-            Ok(Event::Start(ref e)) => {
-                match e.name() {
-                    b"feature" => {
-                        println!(
-                            "attributes values: {:?}",
-                            e.attributes().map(|a| a.unwrap().value).collect::<Vec<_>>()
-                        )
-                    },
-                    _ => (),
-                }
-            },
-            Ok(Event::Text(e)) => {
-                txt.push(e.unescape_and_decode(&reader).unwrap())
-            },
-            Ok(Event::Eof) => {
-                break // exits the loop when reaching end of file
-            },
-            Err(e) => {
-                panic!("Error at position {}: {:?}", reader.buffer_position(), e)
-            },
-            _ => (), // There are several other `Event`s we do not consider here
-        }
-
-        buf.clear();
-    }
-
-    println!("{:?}", txt);
+    println!("{:#?}", root);
 
     Ok(())
 }
