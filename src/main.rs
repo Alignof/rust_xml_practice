dc819e9552fe622c0b9e2353687a5812658803ce
Alingof <Alignof@outlook.com>
2021-12-23 18:24:50 +0900

    [update] convert the namespace to upper case

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-fn set_location(feature: &minidom::Element, namespace: &str) -> Location {
-    let location = feature.get_child("location", namespace).unwrap();
-    dbg!(location);
-    let position = match location.get_child("position", namespace) {
+fn set_location(feature: &minidom::Element, NAMESPACE: &str) -> Location {
+    let location = feature.get_child("location", NAMESPACE).unwrap();
+    let position = match location.get_child("position", NAMESPACE) {
         Some(e) => e.attr("position").map(|p| p.parse::<u32>().unwrap()),
         None => None,
     };
-    let begin = match location.get_child("begin", namespace) {
+    let begin = match location.get_child("begin", NAMESPACE) {
         Some(e) => e.attr("begin").map(|p| p.parse::<u32>().unwrap()),
         None => None,
     };
-    let end = match location.get_child("end", namespace) {
+    let end = match location.get_child("end", NAMESPACE) {
         Some(e) => e.attr("end").map(|p| p.parse::<u32>().unwrap()),
         None => None,
     };
@@ -21,14 +20,14 @@ fn set_location(feature: &minidom::Element, namespace: &str) -> Location {
     }
 }
 
-fn set_feature(feature: &minidom::Element, namespace: &str) -> Feature {
+fn set_feature(feature: &minidom::Element, NAMESPACE: &str) -> Feature {
     Feature {
         f_type: feature.attr("type").unwrap().to_string(),
         description: feature.attr("description").unwrap().to_string(),
         evidence: feature.attr("evidence").map(|s| s.to_string()),
-        original: feature.get_child("original", namespace).map(|e| e.text()),
-        variation: feature.get_child("variation", namespace).map(|e| e.text()),
-        location: set_location(feature, namespace),
+        original: feature.get_child("original", NAMESPACE).map(|e| e.text()),
+        variation: feature.get_child("variation", NAMESPACE).map(|e| e.text()),
+        location: set_location(feature, NAMESPACE),
     }
 }
 
@@ -43,16 +42,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = format!("https://www.uniprot.org/uniprot/{}.xml", protain_id);
     let xml = reqwest::get(url).await?.text().await?;
 
-    const namespace: &str = "http://uniprot.org/uniprot";
+    const NAMESPACE: &str = "http://uniprot.org/uniprot";
     let root: minidom::Element = xml.parse().unwrap();
-    let entry: &minidom::Element = root.get_child("entry", namespace).unwrap();
+    let entry: &minidom::Element = root.get_child("entry", NAMESPACE).unwrap();
     let mut features: Vec<Feature> = Vec::new(); 
 
     for child in entry.children() {
-        if child.is("feature", namespace) {
+        if child.is("feature", NAMESPACE) {
             if is_seq_variant(child) {
                 println!("{:#?}", child);
-                features.push(set_feature(child, namespace));
+                features.push(set_feature(child, NAMESPACE));
             }
         }
     }
