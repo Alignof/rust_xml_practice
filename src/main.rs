823aa545c43fe5196031230b2f272215b82e37ad
Alingof <Alignof@outlook.com>
2021-12-23 13:52:17 +0900

    [update] get the child entry

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,19 @@
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let protain_id = "P0DTC2";
     let url = format!("https://www.uniprot.org/uniprot/{}.xml", protain_id);
-
     let xml = reqwest::get(url).await?.text().await?;
 
+    const namespace: &str = "http://uniprot.org/uniprot";
     let root: minidom::Element = xml.parse().unwrap();
+    let entry: &minidom::Element = root.get_child("entry", namespace).unwrap();
 
-    println!("{:#?}", root);
+    println!("{:#?}", entry);
+
+    for child in entry.children() {
+        if child.is("feature", namespace) {
+            println!("{:#?}", child);
+        }
+    }
 
     Ok(())
 }
