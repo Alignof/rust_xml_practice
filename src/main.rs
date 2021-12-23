ef04ef68bf6a113410ef186b502f536efc6260d4
Alingof <Alignof@outlook.com>
2021-12-23 21:12:11 +0900

    [add] add function write_to_file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-fn set_location(feature: &minidom::Element, NAMESPACE: &str) -> Location {
-    let location = feature.get_child("location", NAMESPACE).unwrap();
-    let position = match location.get_child("position", NAMESPACE) {
+use std::io::Write;
+use std::fs::File;
+
+fn set_location(feature: &minidom::Element, namespace: &str) -> Location {
+    let location = feature.get_child("location", namespace).unwrap();
+    let position = match location.get_child("position", namespace) {
         Some(e) => e.attr("position").map(|p| p.parse::<u32>().unwrap()),
         None => None,
     };
-    let begin = match location.get_child("begin", NAMESPACE) {
+    let begin = match location.get_child("begin", namespace) {
         Some(e) => e.attr("begin").map(|p| p.parse::<u32>().unwrap()),
         None => None,
     };
-    let end = match location.get_child("end", NAMESPACE) {
+    let end = match location.get_child("end", namespace) {
         Some(e) => e.attr("end").map(|p| p.parse::<u32>().unwrap()),
         None => None,
     };
@@ -20,20 +23,54 @@ fn set_location(feature: &minidom::Element, NAMESPACE: &str) -> Location {
     }
 }
 
-fn set_feature(feature: &minidom::Element, NAMESPACE: &str) -> Feature {
+fn set_feature(feature: &minidom::Element, namespace: &str) -> Feature {
     Feature {
-        f_type: feature.attr("type").unwrap().to_string(),
-        description: feature.attr("description").unwrap().to_string(),
-        evidence: feature.attr("evidence").map(|s| s.to_string()),
-        original: feature.get_child("original", NAMESPACE).map(|e| e.text()),
-        variation: feature.get_child("variation", NAMESPACE).map(|e| e.text()),
-        location: set_location(feature, NAMESPACE),
+        f_type: feature
+            .attr("type")
+            .unwrap()
+            .to_string(),
+        description: feature
+            .attr("description")
+            .unwrap()
+            .to_string(),
+        evidence: feature
+            .attr("evidence")
+            .map(|s| s.to_string()),
+        original: feature
+            .get_child("original", namespace)
+            .map(|e| e.text()),
+        variation: feature
+            .get_child("variation", namespace)
+            .map(|e| e.text()),
+        location: set_location(feature, namespace),
     }
 }
 
 fn is_seq_variant(feature: &minidom::Element) -> bool {
     let f_type = feature.attr("type").unwrap();
     f_type == "sequence variant"
+}
+
+fn write_to_file(path: &str, features: Vec<Feature>) -> std::io::Result<()> {
+    let mut file = File::create(path).unwrap();
+
+    for feature in features {
+        writeln!(
+            &mut file,
+            "{}, {}, {}, {}, {}",
+            feature.description,
+            feature.original.unwrap_or("".to_string()),
+            feature.variation.unwrap_or("".to_string()),
+            feature.location.begin
+                .map(|x| format!("{}", x))
+                .unwrap_or("".to_string()),
+            feature.location.end
+                .map(|x| format!("{}", x))
+                .unwrap_or("".to_string()),
+        )?;
+    }
+
+    Ok(())
 }
 
 #[tokio::main]
@@ -50,21 +87,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     for child in entry.children() {
         if child.is("feature", NAMESPACE) {
             if is_seq_variant(child) {
-                println!("{:#?}", child);
                 features.push(set_feature(child, NAMESPACE));
             }
         }
     }
 
+    let path: &str = "./variations.csv";
+    write_to_file(path, features)
+        .expect("writing error.");
+
     Ok(())
 }
 
+#[derive(Debug)]
 struct Location {
     position: Option<u32>,
     begin: Option<u32>,
     end: Option<u32>,
 }
 
+#[derive(Debug)]
 struct Feature {
     f_type: String,
     description: String,
